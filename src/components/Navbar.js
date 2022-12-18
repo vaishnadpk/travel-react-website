59eb5d6342146bf1c7cad25837630a01c7c1ca49
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-18 09:15:02 +0100

    created component and imported images

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <div className="nav-container"></div>
+    </nav>
+  );
+}
+
+export default Navbar;
