ab58fcfa46f4f9ffcc86ab0ef3e809f3c2547075
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-18 11:47:59 +0100

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,11 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="Navbar">
-      <div className="nav-container"></div>
+      <div className="nav-container">
+        <link to="/" className="navbar-logo">
+          TRVL
+        </link>
+      </div>
     </nav>
   );
 }
