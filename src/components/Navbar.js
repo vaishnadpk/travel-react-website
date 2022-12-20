3529a442f20325a71bc6b9c0347e2412a00ac606
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-20 18:44:03 +0100

    updated nav css,button component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Button from "./Button";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+  const showButton = () => {
+    if (window.innerWidth >= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
 
   return (
     <nav className="Navbar">
@@ -50,6 +61,7 @@ function Navbar() {
             </Link>
           </li>
         </ul>
+        {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
       </div>
     </nav>
   );
